aa06c8a1d3f4a0fce4ab5df34f2a92d9e8b23380
joshbooker <email@joshuabooker.com>
2015-10-30 11:21:14 -0400

    add data

---
+++ HTML01/HTML01.HTMLClient/Screens/Tiles/ViewTile.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewTile.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Tile.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Tile." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
